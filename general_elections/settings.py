80ca186361439f285705b3453ab51c72616da98f
Alex Medina <79346881+alexcodes11@users.noreply.github.com>
2022-10-10 03:10:38 -0700

    Styling my Website

--- general_elections/settings.py
+++ general_elections/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-trp%e=ot3_uej*@x24#^2hh0li3@c@o(iri79en8pjg59$kg)q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'general-election.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.121', 'general-election.herokuapp.com']
 
 # Application definition
 
@@ -130,9 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- transportation/views.py
+++ transportation/views.py
@@ -1,15 +1,28 @@
+from itertools import count
 from unittest import result
 from django.shortcuts import render
 from .models import Transportation, Politician
 
 
+
+
+
 def index(request):
     if 'Washington' in request.POST:
-        results = Politician.objects.filter(state = 'Washington')
-        return render(request, "transportation/state.html", {'results': results, 'state': 'Washington', })
+
+        results = Politician.objects.filter(state = 'Washington').values_list('district', flat=True).distinct()
+        group_by_district = {}
+        for value in results:
+            group_by_district[value] = Politician.objects.filter(state = 'Washington', district = value)
+        return render(request, "transportation/state.html", {'results': group_by_district, 'state': 'Washington', })
+
     elif 'Hawaii' in request.POST:
-        results = Politician.objects.filter(state = 'Hawaii')
-        return render(request, "transportation/state.html", {'results': results})
+        results = Politician.objects.filter(state = 'Hawaii').values_list('district', flat=True).distinct()
+        group_by_district = {}
+        for value in results:
+            group_by_district[value] = Politician.objects.filter(state = 'Hawaii', district = value)
+        return render(request, "transportation/state.html", {'results': group_by_district, 'state': 'Hawaii'})
+
     else:
         return render(request, "transportation/index.html")
 
