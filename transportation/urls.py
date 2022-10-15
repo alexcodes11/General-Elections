6d8b2f774b85d225036bda52830a036fdab88041
Alex Medina <79346881+alexcodes11@users.noreply.github.com>
2022-10-15 02:58:07 -0700

    Adding USA MAP and Adding more SQL queries to display all politicians that believe in Public Transportation within the state

--- transportation/urls.py
+++ transportation/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("quotes/<int:quotes_id>", views.quotes, name="quotes")
+    path("quotes/<int:quotes_id>", views.quotes, name="quotes"),
+    path("state/<str:state_name>", views.state, name="state"),
+    path("protransit/<str:state_name>", views.protransit, name="protransit")
 ]
--- transportation/views.py
+++ transportation/views.py
@@ -8,32 +8,24 @@ from .models import Transportation, Politician
 
 
 def index(request):
-    if 'Washington' in request.POST:
+    return render(request, "transportation/index.html")
 
-        results = Politician.objects.filter(state = 'Washington').values_list('district', flat=True).distinct()
-        group_by_district = {}
-        for value in results:
-            group_by_district[value] = Politician.objects.filter(state = 'Washington', district = value)
-        return render(request, "transportation/state.html", {'results': group_by_district, 'state': 'Washington', })
+def protransit(request, state_name):
+    results = Politician.objects.filter(state = state_name, transportation__info_or_not = "Pro Transit").values_list('district', flat=True).distinct()
+    group_by_district = {}
+    for value in results:
+        group_by_district[value] = Politician.objects.filter(state = state_name, district = value, transportation__info_or_not = "Pro Transit")
+    return render(request, "transportation/protransit.html", {'results': group_by_district, 'state': state_name}) 
 
-    elif 'Hawaii' in request.POST:
-        results = Politician.objects.filter(state = 'Hawaii').values_list('district', flat=True).distinct()
-        group_by_district = {}
-        for value in results:
-            group_by_district[value] = Politician.objects.filter(state = 'Hawaii', district = value)
-        return render(request, "transportation/state.html", {'results': group_by_district, 'state': 'Hawaii'})
-
-    else: # We can get each individual state and load each state into the index.html page. 
-        allstates = Politician.objects.values_list('state') 
-        return render(request, "transportation/index.html", {'states': allstates})
-
-def protransit(request):
-    pass 
+def state(request, state_name):
+    results = Politician.objects.filter(state = state_name).values_list('district', flat=True).distinct()
+    group_by_district = {}
+    for value in results:
+        group_by_district[value] = Politician.objects.filter(state = state_name, district = value)
+    return render(request, "transportation/state.html", {'results': group_by_district, 'state': state_name })
 
 def quotes(request, quotes_id):
     if request.method == "GET":
         person = Politician.objects.values_list('full_name', flat=True).get(pk = quotes_id)
         quotes = Transportation.objects.filter(politician__pk = quotes_id)
         return render(request, "transportation/viewmore.html", {"person": person, "quotes": quotes})
-
-
